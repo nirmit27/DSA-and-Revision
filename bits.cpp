20755eb921077023a235c91f97dec40671ad4555
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2022-11-19 06:14:31 +0530

    2 more problems

---
+++ bits.cpp
@@ -0,0 +1,22 @@
+// Program to count the numcounter of 1's in the countinary representation of any numcounter
+#include <bits/stdc++.h> 
+#include <iostream>
+
+using namespace std;
+
+int countcountits(int n){
+    int count=0;
+    while(n){
+        count+=n&1;
+        n>>=1;
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    cout<<"Enter the numcounter: ";
+    cin >> n;
+    cout<<countcountits(n);
+    return 0;
+}
