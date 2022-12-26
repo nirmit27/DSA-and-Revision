118052d366f2b22ab0232ef7a209f9bdb847c2d6
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2022-12-27 00:48:30 +0530

    Codechef moment

---
+++ C++ programs/Lucky_Num.cpp
@@ -0,0 +1,33 @@
+// Program to determine the Lucky numbers from the given test cases (from CodeChef!)
+#include <bits/stdc++.h>
+using namespace std;
+
+void luc_num(int a)
+{
+    int c = 0;
+    if (a % 2 != 0)
+        cout << '\t' << 1 << endl;
+        else
+        {
+            while (a % 2 == 0)
+            { // 48 // 24 // 12 // 6 // 3, stop! // c : 1,2,3,4
+                a /= 2;
+                c++;
+            }
+            cout << '\t' << (c % 2 == 0) << endl;
+        }
+}
+
+int main()
+{
+    int n, x;
+    cout << "\n Enter the number of test cases : ";
+    cin >> n;
+    cout << "\n Enter the values : ";
+    while (n--)
+    {
+        cin >> x;
+        luc_num(x);
+    }
+    return 0;
+}
