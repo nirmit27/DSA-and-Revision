db892529c8f9daea7dc9635c00d59d4703d5db18
Nirmit Mishra <93557744+nirmit27@users.noreply.github.com>
2024-04-04 20:18:34 +0530

    Update tree_traversals.cpp

--- C++ programs/tree_traversals.cpp
+++ C++ programs/tree_traversals.cpp
@@ -7,19 +7,56 @@ public:
   Node *left;
   Node *right;
 
-  Node(int data) : value(data), left((Node *)NULL), right((Node *)NULL){};
+  Node() : value(0), left((Node *)NULL), right((Node *)NULL) {}
+  Node(int data) : value(data), left((Node *)NULL), right((Node *)NULL) {}
+  Node(int data, Node *leftNode, Node *rightNode)
+      : value(data), left(leftNode), right(rightNode) {}
 };
 
+Node *buildTree(Node *root) {
+  int d;
+  cout << endl << "Enter the data for node : ";
+  cin >> d;
+
+  root = new Node(d);
+
+  if (d == -1)
+    return NULL;
+
+  cout << endl << "Enter data for LEFT sub-tree to node " << d << " :" << endl;
+  root->left = buildTree(root->left);
+
+  cout << endl << "Enter data for RIGHT sub-tree to node " << d << " :" << endl;
+  root->right = buildTree(root->right);
+
+  return root;
+}
+
 void postOrderTraversal(Node *root) {
-  //
+  if (!root)
+    return;
+
+  postOrderTraversal(root->left);
+  postOrderTraversal(root->right);
+  cout << root->value << " ";
 }
 
 void preOrderTraversal(Node *root) {
-  //
+  if (!root)
+    return;
+
+  cout << root->value << " ";
+  postOrderTraversal(root->left);
+  postOrderTraversal(root->right);
 }
 
 void inOrderTraversal(Node *root) {
-  //
+  if (!root)
+    return;
+
+  inOrderTraversal(root->left);
+  cout << root->value << " ";
+  inOrderTraversal(root->right);
 }
 
 void levelOrderTraversal(Node *root) {
@@ -50,43 +87,43 @@ void levelOrderTraversal(Node *root) {
   }
 }
 
-Node *buildTree(Node *root) {
-  int d;
-  cout << endl << "Enter the data for node : ";
-  cin >> d;
-
-  root = new Node(d);
-
-  if (d == -1)
-    return NULL;
-
-  cout << endl << "Enter data for LEFT sub-tree to node " << d << " :" << endl;
-  root->left = buildTree(root->left);
-
-  cout << endl << "Enter data for RIGHT sub-tree to node " << d << " :" << endl;
-  root->right = buildTree(root->right);
-
-  return root;
-}
-
 int main() {
+  int ch;
   Node *root = NULL;
 
   // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
   cout << endl << "Building the Tree ... (enter -1 to stop)" << endl;
   root = buildTree(root);
 
-  cout << endl << endl << "Level Order Traversal ..." << endl << endl;
-  levelOrderTraversal(root);
+  cout << endl
+       << endl
+       << "Traversal Menu :\n\n1) Level-Order Traversal\n2) In-Order "
+          "Traversal\n3) Pre-Order Traversal\n4) Post-Order Traversal"
+       << endl
+       << endl
+       << "Enter your choice : ";
+  cin >> ch;
 
-  // cout << endl << endl << "In-order Traversal ..." << endl << endl;
-  // inOrderTraversal(root);
-
-  // cout << endl << endl << "Pre-order Traversal ..." << endl << endl;
-  // preOrderTraversal(root);
-
-  // cout << endl << endl << "Post-order Traversal ..." << endl << endl;
-  // postOrderTraversal(root);
+  switch (ch) {
+  case 1:
+    cout << endl << endl << "Level Order Traversal ..." << endl << endl;
+    levelOrderTraversal(root);
+    break;
+  case 2:
+    cout << endl << endl << "In-order Traversal ..." << endl << endl;
+    inOrderTraversal(root);
+    break;
+  case 3:
+    cout << endl << endl << "Pre-order Traversal ..." << endl << endl;
+    preOrderTraversal(root);
+    break;
+  case 4:
+    cout << endl << endl << "Post-order Traversal ..." << endl << endl;
+    postOrderTraversal(root);
+    break;
+  default:
+    cout << endl << endl << "Wrong choice!" << endl << endl;
+  }
 
   return 0;
 }
