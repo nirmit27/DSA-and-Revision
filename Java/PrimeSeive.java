ef5c31d59b41556b86f6816a7f16364eb1c290e8
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-04-13 19:23:49 +0530

    Arrays.fill( ) method

--- Java/PrimeSeive.java
+++ Java/PrimeSeive.java
@@ -11,8 +11,7 @@ public class PrimeSeive {
         }
 
         boolean[] isPrime = new boolean[n + 1];
-        for (int i = 0; i < isPrime.length; i++)
-            isPrime[i] = true;
+        Arrays.fill(isPrime, true);
 
         for (int i = 2; (i * i) <= n; i++) {
             if (isPrime[i]) {
