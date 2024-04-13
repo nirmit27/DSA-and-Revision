97587322b57b6d3ff33e0f90c98d985fd19ff924
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-04-13 19:25:31 +0530

    fixed the imports

--- Java/PrimeSeive.java
+++ Java/PrimeSeive.java
@@ -1,6 +1,7 @@
 
 /* Implementation of Sieve of Eratosthenes */
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class PrimeSeive {
     public static void main(String[] args) {
