a6d6915c3b0245500118ef4ba49a912b14376b92
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-07-12 23:36:09 +0530

    Ltcd #33 solved

--- Java/DSA/Pivot.java
+++ Java/DSA/Pivot.java
@@ -13,14 +13,20 @@ public class Pivot {
         while (start < end) {
             int mid = start + (end - start) / 2;
 
-            if (mid < end && arr.get(mid) > arr.get(mid + 1)) { // Case 1
+            // Case 1 : Pivot lies in between `start` and `end` and BEFORE `end`
+            if (mid < end && arr.get(mid) > arr.get(mid + 1)) {
                 return mid;
             }
-            if (mid > start && arr.get(mid) < arr.get(mid - 1)) { // Case 2
+            // Case 2 : Pivot lies in between `start` and `end` and AFTER `start`
+            if (mid > start && arr.get(mid) < arr.get(mid - 1)) {
                 return mid - 1;
-            } else if (arr.get(mid) <= arr.get(start)) { // Case 3
+            }
+            // Case 3 : Look for the pivot in the 1st sorted sub-array
+            else if (arr.get(mid) <= arr.get(start)) {
                 end = mid - 1;
-            } else { // Case 4
+            }
+            // Case 4 : Look for the pivot in the 2nd sorted sub-array
+            else {
                 start = mid + 1;
             }
         }
@@ -29,7 +35,7 @@ public class Pivot {
 
     public static void main(String[] args) {
         int size, result;
-        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<Integer> arr = new ArrayList<>(); // {4 5 6 7 0 1 2} -> 7 is the PIVOT.
 
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("\nEnter the array size : ");
--- Java/DSA/SearchRotate.java
+++ Java/DSA/SearchRotate.java
@@ -7,9 +7,63 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SearchRotate {
+    public static int binarySearch(ArrayList<Integer> arr, int start, int end, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr.get(mid) == target) {
+                return mid;
+            } else if (arr.get(mid) > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int findPivot(ArrayList<Integer> arr) {
+        int start = 0, end = arr.size() - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            // Case 1 : Pivot lies in between `start` and `end` and BEFORE `end`
+            if (mid < end && arr.get(mid) > arr.get(mid + 1)) {
+                return mid;
+            }
+            // Case 2 : Pivot lies in between `start` and `end` and AFTER `start`
+            if (mid > start && arr.get(mid) < arr.get(mid - 1)) {
+                return mid - 1;
+            }
+            // Case 3 : Look for the pivot in the 1st sorted sub-array
+            else if (arr.get(mid) <= arr.get(start)) {
+                end = mid - 1;
+            }
+            // Case 4 : Look for the pivot in the 2nd sorted sub-array
+            else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
     static int searchRotate(ArrayList<Integer> arr, int n) {
-        // TODO: Complete the LeetCode question!!!
- 
+        if (arr.size() == 0) {
+            return -1;
+        }
+        int pivot = findPivot(arr);
+
+        if (pivot == -1) {
+            return binarySearch(arr, 0, arr.size() - 1, n);
+        }
+
+        if (arr.get(pivot) == n) {
+            return pivot;
+        } else if (n >= arr.get(0)) {
+            return binarySearch(arr, 0, pivot - 1, n);
+        } else if (n < arr.get(0)) {
+            return binarySearch(arr, pivot + 1, arr.size() - 1, n);
+        }
         return -1;
     }
 
