4d1439f98ad1964917f9f9a9cab85342549e237a
Nirmit Mishra <93557744+nirmit27@users.noreply.github.com>
2024-12-05 03:53:15 +0530

    Create MinRepSubstring.java

---
+++ Java/DSA/MinRepSubstring.java
@@ -0,0 +1,83 @@
+/* Calculating the minimum repetitions of a string `s1` to make `s2` a substring */
+
+import java.util.Scanner;
+
+public class MinRepSubstring {
+    static void computeLPSArray(String s, int[] lps) {
+        int l = 0, i = 1;
+        lps[0] = 0;
+
+        while (i < s.length()) {
+            if (s.charAt(i) == s.charAt(l)) {
+                l++;
+                lps[i] = l;
+                i++;
+            } 
+            else {
+                if (l != 0) 
+                    l = lps[l - 1];
+                else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
+    static boolean kmpSearch(String txt, String pat, int[] lps, int reps) {
+        int n = txt.length(), m = pat.length();
+        int i = 0, j = 0;
+
+        while (i < n * reps) {
+            if (txt.charAt(i % n) == pat.charAt(j)) {
+                i++;
+                j++;
+
+                if (j == m)
+                    return true;
+            } 
+            else {
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i++;
+            }
+        }
+        return false;
+    }
+
+    static int minReps(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        
+        int[] lps = new int[m];
+        computeLPSArray(s2, lps);
+        
+        int x = (m + n - 1) / n;
+        
+        if (kmpSearch(s1, s2, lps, x))
+            return x;
+
+        if (kmpSearch(s1, s2, lps, x + 1))
+            return x + 1;
+
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+        int res;
+        String s1, s2;
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the first string : ");
+            s1 = sc.nextLine();
+            System.out.print("Enter the second string : ");
+            s2 = sc.nextLine();
+        }
+        res = minReps(s1, s2);
+        if (res != -1)
+            System.out.println(String.format("\nNumber of repetitions : %d", res));
+        else
+            System.out.println("\nNo solution found!");
+    }
+}
