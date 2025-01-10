12ddc4f2a443a96c0758b95e011c34a374f1a80e
--local <mishrayyhsnm2002@gmail.com>
2025-01-11 02:09:19 +0530

    Correction for Ltcd #3 solution

--- Java/DSA/LeetCode/LongSubNoRep.java
+++ Java/DSA/LeetCode/LongSubNoRep.java
@@ -4,38 +4,44 @@ LeetCode problem #3 solution
 Link : https://leetcode.com/problems/longest-substring-without-repeating-characters/
  */
 
-import java.lang.Math;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class LongSubNoRep {
-    public static int subNoRep(String s) {
-        if (s.length() == 0)
-            return 0;
-        int maxLength = 0;
-        int left = 0;
+    public static int ptr = 0;
 
-        for (int right = 0; right < s.length(); right++) {
-            for (int j = 0; j < right; j++) {
-                if (s.charAt(j) == s.charAt(right))
-                    left = j + 1;
+    public static int subNoRep(String s) {
+        if (s.length() < 1)
+            return s.length();
+
+        int ans = 0, left = 0, right = 0;
+        HashSet<Character> set = new HashSet<>();
+
+        for (right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
             }
-            maxLength = Math.max(maxLength, right - left + 1);
+            set.add(s.charAt(right));
+
+            if (ans < (right - left + 1)) {
+                ans = right - left + 1;
+                ptr = left;
+            }
         }
-        return maxLength;
+        return ans;
     }
 
     public static void main(String[] args) {
-        int result = 0;
+        int result;
         String input;
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("\nEnter the string : ");
             input = sc.nextLine();
         }
         result = subNoRep(input);
-        if (result != 0)
-            System.out
-                    .println(String.format("\nLength of longest substring without repeating characters : %d", result));
-        else
-            System.out.println("\nA substring without repeating characters DOESN'T exist.");
+        System.out
+                .println(String.format("\nLength of the longest substring without repeating characters : %s",
+                        input.substring(ptr, ptr + result)));
     }
 }
