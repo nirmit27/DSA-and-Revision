72a6ead8df397e9af86d6d74aebd1d53efbcad46
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2025-01-11 02:28:24 +0530

    Implement optimized approach for finding the length of the longest substring without repeating characters; update README for clarity

--- Java/DSA/LeetCode/LongSubNoRep.java
+++ Java/DSA/LeetCode/LongSubNoRep.java
@@ -5,12 +5,35 @@ Link : https://leetcode.com/problems/longest-substring-without-repeating-charact
  */
 
 import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class LongSubNoRep {
     public static int ptr = 0;
 
-    public static int subNoRep(String s) {
+    public static int mapApproach(String s) {
+        if (s.length() < 1)
+            return s.length();
+
+        int ans = 0, left = 0, right = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (right = 0; right < s.length(); right++) {
+            map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
+            while (map.get(s.charAt(right)) > 1) {
+                map.put(s.charAt(left), map.getOrDefault(s.charAt(left), 0) - 1);
+                left++;
+            }
+
+            if (ans < (right - left + 1)) {
+                ans = right - left + 1;
+                ptr = left;
+            }
+        }
+        return ans;
+    }
+
+    public static int setApproach(String s) {
         if (s.length() < 1)
             return s.length();
 
@@ -39,7 +62,7 @@ public class LongSubNoRep {
             System.out.print("\nEnter the string : ");
             input = sc.nextLine();
         }
-        result = subNoRep(input);
+        result = mapApproach(input);
         System.out
                 .println(String.format("\nLength of the longest substring without repeating characters : %s",
                         input.substring(ptr, ptr + result)));
