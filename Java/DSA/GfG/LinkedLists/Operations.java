c30a4a690c38a8598b2844d795341d473830e63e
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2025-01-19 08:31:06 +0530

    Refactor linked list operations: consolidate reversal and add rotation functionality

--- Java/DSA/GfG/LinkedLists/Operations.java
+++ Java/DSA/GfG/LinkedLists/Operations.java
@@ -1,12 +1,46 @@
 package GfG.LinkedLists;
 /* 
-GfG problem - "Reverse a linked list" solution
-Link : https://www.geeksforgeeks.org/problems/reverse-a-linked-list/1
+    --- GfG Problems ---
+    #1 : "Reverse a linked list" solution
+    Link : https://www.geeksforgeeks.org/problems/reverse-a-linked-list/1    
+
+    #2 : "Rotate a Linked List" solution
+    Link : https://www.geeksforgeeks.org/problems/rotate-a-linked-list/1
  */
 
 import java.util.Stack;
 
-public class ReverseList {
+public class Operations {
+    /* Problem #2 - Rotation */
+    public Node rotateList(Node head, int k) {
+        if (head == null || head.next == null || k == 0)
+            return head;
+
+        Node temp = head;
+        int length = 1;
+
+        // Determine the length
+        while (temp.next != null) {
+            temp = temp.next;
+            length++;
+        }
+
+        // Locate new position of HEAD
+        k %= length;
+        temp.next = head;
+
+        while (k > 0) {
+            temp = temp.next;
+            k--;
+        }
+
+        // Perform rotation
+        head = temp.next;
+        temp.next = null;
+        return head;
+    }
+
+    /* Problem #1 - Reversal */
     // ITERATIVE Approach
     public Node reverseListI(Node head) {
         Node prev = null;
@@ -21,7 +55,6 @@ public class ReverseList {
             prev = curr;
             curr = next;
         }
-
         return prev;
     }
 
@@ -56,7 +89,6 @@ public class ReverseList {
                 temp.next = stack.pop();
                 temp = temp.next;
             }
-
             temp.next = null;
         }
         return head;
