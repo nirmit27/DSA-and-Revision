4ff225e63bb000ba09fb37010b14cc4c4702769d
Nirmit Mishra <93557744+nirmit27@users.noreply.github.com>
2025-07-01 05:38:25 +0530

    Ltcd #696 solution

---
+++ Python/adeptus/astartes/cbs.py
@@ -0,0 +1,30 @@
+"""
+[Leetcode] #696 Count Binary Substrings
+Link: https://leetcode.com/problems/count-binary-substrings/
+"""
+
+
+class CBS:
+    def countBinarySubstrings(self, s: str) -> int:
+        ans: int = 0
+        pc: int = 0
+        cc: int = 1
+
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                cc += 1
+            else:
+                ans += min(cc, pc)
+                pc = cc
+                cc = 1
+
+        # For the last valid substring ...
+        ans += min(pc, cc)
+        return ans
+
+
+def main() -> None: ...
+
+
+if __name__ == "__main__":
+    main()
